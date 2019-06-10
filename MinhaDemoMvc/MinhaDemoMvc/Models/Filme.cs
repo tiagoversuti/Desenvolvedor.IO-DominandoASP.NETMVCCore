7f481c0465ffbbb1422c2f9374b05b3a550a6fb5
Tiago Versuti <tiagoversuti@gmail.com>
2019-06-09 23:43:00 -0300

    Trabalhando com Models

---
+++ MinhaDemoMvc/MinhaDemoMvc/Models/Filme.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhaDemoMvc.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public DateTime DataLancamento { get; set; }
+        public string Genero { get; set; }
+        public decimal Valor { get; set; }
+        public int Avaliacao { get; set; }
+    }
+}
