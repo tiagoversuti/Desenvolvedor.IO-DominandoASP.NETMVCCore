14ec8ba6364c614381ba1b22c437905e6c3181a6
Tiago Versuti <tiagoversuti@gmail.com>
2019-06-09 23:28:27 -0300

    Trabalhando com Action Results

--- MinhaDemoMvc/MinhaDemoMvc/Controllers/HomeController.cs
+++ MinhaDemoMvc/MinhaDemoMvc/Controllers/HomeController.cs
@@ -8,27 +8,24 @@ using MinhaDemoMvc.Models;
 
 namespace MinhaDemoMvc.Controllers
 {
-    [Route("")]
-    [Route("gestao-clientes")]
     public class HomeController : Controller
     {
-        [Route("")]
-        [Route("pagina-inicial")]
-        [Route("pagina-inicial/{id:int}/{categoria:guid}")]
         public IActionResult Index(string Id, Guid categoria)
         {
             return View();
         }
 
-        [Route("privacidade")]
-        [Route("politica-de-privacidade")]
         public IActionResult Privacy()
         {
-            return View();
+            //return Json("{'nome':'Tiago'}");
+            //var fileBytes = System.IO.File.ReadAllBytes(@"F:\INSTALAR.txt");
+            //var fileName = "ola.txt";
+            //return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+
+            return Content("Qualquer coisa");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        [Route("erro-encontrado")]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
