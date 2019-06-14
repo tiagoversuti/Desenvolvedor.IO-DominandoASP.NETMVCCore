fcc4b5319b97756373113719e8b297298f661c2d
Tiago Versuti <tiagoversuti@gmail.com>
2019-06-14 00:20:59 -0300

    Ganhe tempo. Utilize Scaffold

---
+++ MinhaDemoMvc/MinhaDemoMvc/Data/MinhaDemoMvcContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MinhaDemoMvc.Models
+{
+    public class MinhaDemoMvcContext : DbContext
+    {
+        public MinhaDemoMvcContext (DbContextOptions<MinhaDemoMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MinhaDemoMvc.Models.Filme> Filme { get; set; }
+    }
+}
--- MinhaDemoMvc/MinhaDemoMvc/Startup.cs
+++ MinhaDemoMvc/MinhaDemoMvc/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using MinhaDemoMvc.Models;
 
 namespace MinhaDemoMvc
 {
@@ -33,6 +35,9 @@ namespace MinhaDemoMvc
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<MinhaDemoMvcContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MinhaDemoMvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
