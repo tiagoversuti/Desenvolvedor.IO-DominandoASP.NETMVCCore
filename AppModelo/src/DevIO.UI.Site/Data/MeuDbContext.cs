1e9bc0c03b1a0d25947201c805bffce8e1646974
Tiago Versuti <tiagoversuti@gmail.com>
2019-06-25 00:36:41 -0300

    Configurando o DbContext

---
+++ AppModelo/src/DevIO.UI.Site/Data/MeuDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.UI.Site.Data
+{
+    public class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions<MeuDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
