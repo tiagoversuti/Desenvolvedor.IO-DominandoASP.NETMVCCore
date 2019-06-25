9819a7fdf61c89aa627791247929903468989ea3
Tiago Versuti <tiagoversuti@gmail.com>
2019-06-25 00:48:28 -0300

    Operações CRUD

--- AppModelo/src/DevIO.UI.Site/Data/MeuDbContext.cs
+++ AppModelo/src/DevIO.UI.Site/Data/MeuDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DevIO.UI.Site.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,7 @@ namespace DevIO.UI.Site.Data
         {
 
         }
+
+        public DbSet<Aluno> Alunos { get; set; }
     }
 }
