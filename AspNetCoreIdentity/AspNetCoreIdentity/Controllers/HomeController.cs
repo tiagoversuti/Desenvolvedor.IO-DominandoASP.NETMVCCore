7d1c3b0aad6d80e673cdd743384a956a691f8a5a
Tiago Versuti <tiagoversuti@gmail.com>
2019-06-26 01:14:22 -0300

    Segurança com ASP.NET Identity: Autenticação

--- AspNetCoreIdentity/AspNetCoreIdentity/Controllers/HomeController.cs
+++ AspNetCoreIdentity/AspNetCoreIdentity/Controllers/HomeController.cs
@@ -5,11 +5,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AspNetCoreIdentity.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace AspNetCoreIdentity.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
