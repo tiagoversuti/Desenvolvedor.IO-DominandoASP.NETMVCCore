e19e2e2ab0284e5ef365be7a0bfc1e60bcdce688
Tiago Versuti <tiagoversuti@gmail.com>
2019-07-03 00:59:41 -0300

    Tratamento de erros

--- AspNetCoreIdentity/AspNetCoreIdentity/Startup.cs
+++ AspNetCoreIdentity/AspNetCoreIdentity/Startup.cs
@@ -43,7 +43,8 @@ namespace AspNetCoreIdentity
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/erro/500");
+                app.UseStatusCodePagesWithRedirects("/erro/{0}");
                 app.UseHsts();
             }
 
