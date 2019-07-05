f628f0b0fbfad317d13bd9d9ea03a91f9f60228d
Tiago Versuti <tiagoversuti@gmail.com>
2019-07-05 01:50:11 -0300

    Começando da forma fácil

---
+++ AppMvcBasica/AppMvcBasica/Models/Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppMvcBasica.Models
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
